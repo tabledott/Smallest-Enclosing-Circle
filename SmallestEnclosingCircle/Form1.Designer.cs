f59997bca9c2a9139f7879ee415162bbc0b53235
unknown <tabledott@.(none)>
2010-12-26 23:09:24 +0200

    initial commit

---
+++ SmallestEnclosingCircle/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace SmallestEnclosingCircle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainDrawingArea = new System.Windows.Forms.PictureBox();
+            this.DrawCircle = new System.Windows.Forms.Button();
+            this.ClearPoints = new System.Windows.Forms.Button();
+            this.DrawNextStep = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.MainDrawingArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainDrawingArea
+            // 
+            this.MainDrawingArea.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MainDrawingArea.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MainDrawingArea.Location = new System.Drawing.Point(23, 105);
+            this.MainDrawingArea.Name = "MainDrawingArea";
+            this.MainDrawingArea.Size = new System.Drawing.Size(1009, 522);
+            this.MainDrawingArea.TabIndex = 0;
+            this.MainDrawingArea.TabStop = false;
+            this.MainDrawingArea.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainDrawingArea_MouseClick);
+            // 
+            // DrawCircle
+            // 
+            this.DrawCircle.Location = new System.Drawing.Point(76, 33);
+            this.DrawCircle.Name = "DrawCircle";
+            this.DrawCircle.Size = new System.Drawing.Size(109, 37);
+            this.DrawCircle.TabIndex = 1;
+            this.DrawCircle.Text = "DrawCircle";
+            this.DrawCircle.UseVisualStyleBackColor = true;
+            this.DrawCircle.Click += new System.EventHandler(this.DrawCircle_Click);
+            // 
+            // ClearPoints
+            // 
+            this.ClearPoints.Location = new System.Drawing.Point(607, 37);
+            this.ClearPoints.Name = "ClearPoints";
+            this.ClearPoints.Size = new System.Drawing.Size(107, 36);
+            this.ClearPoints.TabIndex = 2;
+            this.ClearPoints.Text = "ClearPoints";
+            this.ClearPoints.UseVisualStyleBackColor = true;
+            this.ClearPoints.Click += new System.EventHandler(this.ClearPoints_Click);
+            // 
+            // DrawNextStep
+            // 
+            this.DrawNextStep.Location = new System.Drawing.Point(363, 37);
+            this.DrawNextStep.Name = "DrawNextStep";
+            this.DrawNextStep.Size = new System.Drawing.Size(89, 33);
+            this.DrawNextStep.TabIndex = 3;
+            this.DrawNextStep.Text = "DrawNextStep";
+            this.DrawNextStep.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 639);
+            this.Controls.Add(this.DrawNextStep);
+            this.Controls.Add(this.ClearPoints);
+            this.Controls.Add(this.DrawCircle);
+            this.Controls.Add(this.MainDrawingArea);
+            this.Name = "Form1";
+            this.Text = "Smallest Enclosing Circle";
+            ((System.ComponentModel.ISupportInitialize)(this.MainDrawingArea)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MainDrawingArea;
+        private System.Windows.Forms.Button DrawCircle;
+        private System.Windows.Forms.Button ClearPoints;
+        private System.Windows.Forms.Button DrawNextStep;
+    }
+}
+
---
+++ SmallestEnclosingCircle/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace SmallestEnclosingCircle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            points = new ArrayList();
+            g = this.MainDrawingArea.CreateGraphics();
+        }
+
+        private void MainDrawingArea_MouseClick(object sender, MouseEventArgs e)
+        {
+            points.Add(new Point(e.X, e.Y));
+            g.DrawRectangle(new Pen(Color.Red), e.X, e.Y, 1, 1);
+        }
+
+        private void DrawCircle_Click(object sender, EventArgs e)
+        {
+            Point centre = (Point)points[0];
+            double dist = GetDistance((Point)points[0], (Point)points[1]);
+            g.DrawArc(new Pen(Color.Red), centre.X, centre.Y, (int)dist, (int)dist, 0, 360);
+        }
+
+        private void ClearPoints_Click(object sender, EventArgs e)
+        {
+            Bitmap cleanBitmap = new Bitmap(this.MainDrawingArea.Width, this.MainDrawingArea.Height);
+            this.MainDrawingArea.Image = cleanBitmap;
+            points.Clear();
+        }
+
+        private double GetDistance(Point first, Point second)
+        {
+            return Math.Sqrt((first.X - second.X) * (first.X - second.X) + (first.Y - second.Y) * (first.Y - second.Y));
+        }
+
+        private ArrayList points;
+        private Graphics g;
+    }
+}
