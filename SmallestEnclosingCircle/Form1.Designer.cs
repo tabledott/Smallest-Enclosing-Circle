83e407c9f48ed428d6a0cfc9379734f5dbf54abf
tabledott <tabledott@gmail.com>
2011-01-05 01:27:01 +0200

    first working version - need to make some fixes.

--- SmallestEnclosingCircle/Form1.Designer.cs
+++ SmallestEnclosingCircle/Form1.Designer.cs
@@ -29,9 +29,7 @@
         private void InitializeComponent()
         {
             this.MainDrawingArea = new System.Windows.Forms.PictureBox();
-            this.DrawCircle = new System.Windows.Forms.Button();
             this.ClearPoints = new System.Windows.Forms.Button();
-            this.DrawNextStep = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.MainDrawingArea)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,50 +37,29 @@
             // 
             this.MainDrawingArea.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.MainDrawingArea.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.MainDrawingArea.Location = new System.Drawing.Point(23, 105);
+            this.MainDrawingArea.Location = new System.Drawing.Point(23, 12);
             this.MainDrawingArea.Name = "MainDrawingArea";
             this.MainDrawingArea.Size = new System.Drawing.Size(1009, 522);
             this.MainDrawingArea.TabIndex = 0;
             this.MainDrawingArea.TabStop = false;
             this.MainDrawingArea.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainDrawingArea_MouseClick);
             // 
-            // DrawCircle
-            // 
-            this.DrawCircle.Location = new System.Drawing.Point(76, 33);
-            this.DrawCircle.Name = "DrawCircle";
-            this.DrawCircle.Size = new System.Drawing.Size(109, 37);
-            this.DrawCircle.TabIndex = 1;
-            this.DrawCircle.Text = "DrawCircle";
-            this.DrawCircle.UseVisualStyleBackColor = true;
-            this.DrawCircle.Click += new System.EventHandler(this.DrawCircle_Click);
-            // 
             // ClearPoints
             // 
-            this.ClearPoints.Location = new System.Drawing.Point(607, 37);
+            this.ClearPoints.Location = new System.Drawing.Point(437, 569);
             this.ClearPoints.Name = "ClearPoints";
             this.ClearPoints.Size = new System.Drawing.Size(107, 36);
-            this.ClearPoints.TabIndex = 2;
+            this.ClearPoints.TabIndex = 3;
             this.ClearPoints.Text = "ClearPoints";
             this.ClearPoints.UseVisualStyleBackColor = true;
-            this.ClearPoints.Click += new System.EventHandler(this.ClearPoints_Click);
-            // 
-            // DrawNextStep
-            // 
-            this.DrawNextStep.Location = new System.Drawing.Point(363, 37);
-            this.DrawNextStep.Name = "DrawNextStep";
-            this.DrawNextStep.Size = new System.Drawing.Size(89, 33);
-            this.DrawNextStep.TabIndex = 3;
-            this.DrawNextStep.Text = "DrawNextStep";
-            this.DrawNextStep.UseVisualStyleBackColor = true;
+            this.ClearPoints.Click += new System.EventHandler(this.ClearPoints_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1044, 639);
-            this.Controls.Add(this.DrawNextStep);
             this.Controls.Add(this.ClearPoints);
-            this.Controls.Add(this.DrawCircle);
             this.Controls.Add(this.MainDrawingArea);
             this.Name = "Form1";
             this.Text = "Smallest Enclosing Circle";
@@ -94,9 +71,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox MainDrawingArea;
-        private System.Windows.Forms.Button DrawCircle;
         private System.Windows.Forms.Button ClearPoints;
-        private System.Windows.Forms.Button DrawNextStep;
     }
 }
 
--- SmallestEnclosingCircle/Form1.cs
+++ SmallestEnclosingCircle/Form1.cs
@@ -17,14 +17,64 @@ namespace SmallestEnclosingCircle
         {
             Point Center;
             double radius;
-            Circle(Point Center, double radius)
+
+            public Circle() 
+            {
+                this.Center = new Point(0, 0);
+                radius = 0;
+            }
+
+            public Circle(Point Center, double radius)
             {
                 this.Center = Center;
                 this.radius = radius;
             }
-            bool belongsToCircle(Point other)
+
+            public Circle(Point Center)
             {
-                return Math.Abs(GetDistance(Center, other) - radius) < 0.0001;
+                this.Center = Center;
+                this.radius = 0;
+            }
+
+            public Circle(Point first, Point second)
+            {
+                Center = new Point((first.X + second.X) / 2, (first.Y + second.Y) / 2);
+                radius = GetDistance(first, Center);
+            }
+
+            public Circle(Point p1, Point p2, Point p3)
+            {
+                try
+                {
+                    double x = (p3.X * p3.X * (p1.Y - p2.Y) + (p1.X * p1.X + (p1.Y - p2.Y) * (p1.Y - p3.Y))
+                              * (p2.Y - p3.Y) + p2.X * p2.X * (-p1.Y + p3.Y))
+                              / (2 * (p3.X * (p1.Y - p2.Y) + p1.X * (p2.Y - p3.Y) + p2.X * (-p1.Y + p3.Y)));
+                    double y = (p2.Y + p3.Y) / 2 - (p3.X - p2.X) / (p3.Y - p2.Y) * (x - (p2.X + p3.X) / 2);
+                    Center = new Point((int)x, (int)y);
+                    radius = GetDistance(Center, p1);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Error creating circle with three points: " +   e.ToString());
+                }
+
+            }
+
+            public Point getCenter()
+            {
+                return Center;
+            }
+
+            public double getRadius()
+            {
+                return radius;
+            }
+
+            public int belongsToCircle(Point other)
+            {
+               if( Math.Abs(GetDistance(Center, other) - radius) < 0.0001 ) return 0;
+               if (GetDistance(Center, other) > radius) return -1;
+               return 1;
             }
         }
 
@@ -33,30 +83,80 @@ namespace SmallestEnclosingCircle
             InitializeComponent();
             points = new ArrayList();
             g = this.MainDrawingArea.CreateGraphics();
+            p = new Point[1000];
+            b = new Point[3];
+            sec = new Circle();
+            prev_x = prev_y = prev_width = prev_height = 0;
         }
+
+        private Point[] p;					// points input by the user
+        private int n = 0;					// Number of points input by the user
+        private Point[] b;					// Points on the boundary of the circle
+        private Circle sec;				// Smallest Enclosing Circle
+        private int prev_x, prev_y, prev_width, prev_height;
+
+        private Circle findSec(int n, Point[] p, int m, Point[] b)
+        {
+            Circle sec = new Circle();
+
+            // Compute the Smallest Enclosing Circle defined by B
+            if (m == 1)
+            {
+                sec = new Circle(b[0]);
+            }
+            else if (m == 2)
+            {
+                sec = new Circle(b[0], b[1]);
+            }
+            else if (m == 3)
+            {
+                return new Circle(b[0], b[1], b[2]);
+            }
+
+            // Check if all the points in p are enclosed
+            for (int i = 0; i < n; i++)
+            {   
+                //if the point is outside the circle
+                if (sec.belongsToCircle(p[i]) == -1)
+                {
+                    // Compute B <--- B union P[i].
+                    b[m] = new Point(p[i].X, p[i].Y);
+                    // Recurse
+                    sec = findSec(i, p, m + 1, b);
+                }
+            }
+
+            return sec;
+        }
+
 
         private void MainDrawingArea_MouseClick(object sender, MouseEventArgs e)
         {
             points.Add(new Point(e.X, e.Y));
-            g.DrawRectangle(new Pen(Color.Red), e.X, e.Y, 1, 1);
-        }
-
-        private void DrawCircle_Click(object sender, EventArgs e)
-        {
-            Point centre = (Point)points[0];
-            int r = (int)GetDistance((Point)points[0], (Point)points[1]);
-            int x = centre.X - r;
-            int y = centre.Y - r;
-            int width = 2 * r;
-            int height = 2 * r;
-            g.DrawArc(new Pen(Color.Red), x, y, width, height, 0, 360);
-        }
-
-        private void ClearPoints_Click(object sender, EventArgs e)
-        {
-            Bitmap cleanBitmap = new Bitmap(this.MainDrawingArea.Width, this.MainDrawingArea.Height);
-            this.MainDrawingArea.Image = cleanBitmap;
-            points.Clear();
+            g.DrawArc(new Pen(Color.Red), (float)e.X, (float)e.Y, (float)3.0, (float)3.0, (float)0.0, (float)360.0);
+            p[n++] = new Point(e.X, e.Y);
+            sec = findSec(n, p, 0, b);
+            if (n > 1)
+            {
+                try
+                {
+                    Point center = sec.getCenter();
+                    int r = (int) sec.getRadius();
+                    if (prev_height > 0)
+                    {
+                        g.DrawArc(new Pen(Color.White), prev_x, prev_y, prev_width, prev_height, 0, 360);
+                    }
+                    int x = center.X - r; prev_x = x;
+                    int y = center.Y - r; prev_y = y;
+                    int width = 2 * r; prev_width = width;
+                    int height = 2 * r; prev_height = height;
+                    g.DrawArc(new Pen(Color.Blue), x, y, width, height, 0, 360);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
         }
 
         private static double GetDistance(Point first, Point second)
@@ -66,6 +166,17 @@ namespace SmallestEnclosingCircle
 
         private ArrayList points;
         private Graphics g;
+
+        private void ClearPoints_Click_1(object sender, EventArgs e)
+        {
+            Bitmap cleanBitmap = new Bitmap(this.MainDrawingArea.Width, this.MainDrawingArea.Height);
+            this.MainDrawingArea.Image = cleanBitmap;
+            points.Clear();
+            p = new Point[1000];
+            b = new Point[3];
+            sec = new Circle();
+            n = 0; prev_x = prev_y = prev_height = prev_width = 0;
+        }
 
     }
 }
